e69a1ff9c246b66ab539e50659bef56d30fca1cb
Angeline Lee <aglee92@gmail.com>
2014-04-01 23:27:16 -0700

    initial commit

---
+++ src/cs169/carfollowingapp/test/TestLoginUI.java
@@ -0,0 +1,16 @@
+package cs169.carfollowingapp.test;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+
+public class TestLoginUI extends UiAutomatorTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
